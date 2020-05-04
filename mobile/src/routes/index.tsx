3318af9fe965008e031fc24b8e2e424f8126aa71
Matheus Santos <matheus.psantos2016@gmail.com>
2020-05-04 13:25:15 -0300

    routes modified to tests

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -4,20 +4,24 @@ import { View, ActivityIndicator } from 'react-native';
 import AuthRoutes from '../routes/auth.routes';
 import AppRoutes from '../routes/app.routes';
 
+import Whiteboard from '../screens/Whiteboard';
+
 import { useAuth } from '../contexts/auth';
 
 const Routes: React.FC = () => {
-    const { signed, loading } = useAuth();
+    // const { signed, loading } = useAuth();
 
-    if (loading) {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
-                <ActivityIndicator size='large' color='#666' />
-            </View>
-        );
-    }
+    // if (loading) {
+    //     return (
+    //         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
+    //             <ActivityIndicator size='large' color='#666' />
+    //         </View>
+    //     );
+    // }
 
-    return signed ? <AppRoutes /> : <AuthRoutes />
+    // return signed ? <AppRoutes /> : <AuthRoutes />
+
+    return <Whiteboard/>
 };
 
 export default Routes;
---
+++ mobile/src/screens/Whiteboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Whiteboard: React.FC = () => {
+    return(
+        <View>
+            <Text>Whiteboard</Text>
+        </View>
+    );  
+};
+
+export default Whiteboard;
