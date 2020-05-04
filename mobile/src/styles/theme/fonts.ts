bc0ed5b3c54ec2e6c666dbe03eff6bd905eb5033
Matheus Santos <matheus.psantos2016@gmail.com>
2020-05-04 16:11:47 -0300

    fonts.ts theme file added

--- mobile/src/styles/theme/fonts.ts
+++ mobile/src/styles/theme/fonts.ts
@@ -2,12 +2,12 @@ export default {
     khandBold: 'Khand-Bold',
     khandSemiBold: 'Khand-SemiBold',
     khandMedium: 'Khand-Medium',
-  };
-  
-  export interface FontsProps {
+};
+
+export interface FontsProps {
     khandBold: string;
     khandSemiBold: string;
     khandMedium: string;
-  }
-  
-  export type TextOptions = keyof FontsProps;
+}
+
+export type TextOptions = keyof FontsProps;
